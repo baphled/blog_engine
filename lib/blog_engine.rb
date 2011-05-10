1b3f0e71a8b4bbba0d805e9e7d82e796b35d3a03
Yomi Colledge <baphled@boodah.net>
2011-05-10 14:01:08 +0100

    Initial commit

---
+++ lib/blog_engine.rb
@@ -0,0 +1,2 @@
+module BlogEngine
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'b0ef4f1390c1f364136c4eb0f1fc674755b2e178d4bf45eb1a96ffa1588de5e749fc204be76880d11f9304c47932a0cd590b8e53f5d5a764961f94b75599d9fe'
