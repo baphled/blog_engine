d706e83aea37c05878de6036c741898069d89bda
Yomi Colledge <baphled@boodah.net>
2011-08-11 19:53:11 +0100

    Improvements to layout

--- spec/dummy/public/javascripts/application.js
+++ spec/dummy/public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-  $('#blog_engine_article_content').markItUp(mySettings);
+  $('#blog_engine_article_content, #blog_engine_comment_body').markItUp(mySettings);
   $('#blog_engine_article_published_at').datepicker({ dateFormat: 'dd-mm-yy', gotoCurrent: true, changeYear: true });
 });
 
