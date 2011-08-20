73d705faf136d5df1e3bfcc82fe2d733d36e316e
Yomi Colledge <baphled@boodah.net>
2011-08-20 19:24:31 +0100

    Updated routes so that we user articles instead of blog-engine.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
     :class_name => 'BlogEngine::Author',
     :controllers => { :registrations => "blog_engine/authors/registrations" }
   
-  match '/blog-engine', :controller => 'blog_engine/articles', :action => :articles, :as => :blog_engine_published_articles
+  match '/articles', :controller => 'blog_engine/articles', :action => :articles, :as => :blog_engine_published_articles
   
   authenticate :author do
-    resource 'blog-engine', :controller => 'blog_engine/engine', :as => :blog_engine, :only => [:index] do
+    resource 'articles', :controller => 'blog_engine/engine', :as => :blog_engine, :only => [:index] do
       resources :articles, :controller => 'blog_engine/articles' do
         resources :comments, :controller => 'blog_engine/comments'
       end
@@ -15,12 +15,12 @@ Rails.application.routes.draw do
     end
   end
   
-  match '/blog-engine/tag/:tag',
+  match '/articles/tag/:tag',
     :controller => 'blog_engine/articles',
     :action => :tag,
     :as => :blog_engine_published_articles_by_tag
   
-  match '/blog-engine/:year/:month/:slug',
+  match '/articles/:year/:month/:slug',
     :controller => 'blog_engine/articles',
     :action => :article,
     :as => :blog_engine_published_article
